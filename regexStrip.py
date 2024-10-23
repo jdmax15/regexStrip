9531d7e84d17a5ece7fe1cbbab484af74858687e
jdmax15 <joel.maxwell15@gmail.com>
2024-10-23 11:13:08 +1030

    Initial commit

---
+++ regexStrip.py
@@ -0,0 +1,19 @@
+import re, sys
+
+'''
+1. Configure sys.argv inputs.
+2. Whitespace strip logic
+3. Char strip logic
+
+'''
+
+def main():
+    if len(sys.argv) == 2:
+        None # Strip white space from beginning/end of string.
+
+    elif len(sys.argv) == 3:
+        None # Remove the 2nd arg from 1st input string.
+        
+    else:
+        sys.exit('CLI Input Error, have either 1 or 2 additional args. ')
+
